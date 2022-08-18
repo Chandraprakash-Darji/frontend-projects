6e7504d6fc9aff5134b067532c50e3de2b659758
Chandraprakash Darji <prakashchandra3786@gmail.com>
2022-08-18 20:19:57 +0530

    animated cursor for Hero section

---
+++ basicagency/src/components/HeroCursor/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useRef, useState } from "react";
+
+interface Props {
+    text: string;
+    subtitle: string;
+    dragable?: boolean;
+    heroRef: React.RefObject<HTMLDivElement>;
+    onClick: () => void;
+}
+
+const HeroCursor = ({ text, subtitle, heroRef, onClick }: Props) => {
+    const [positon, setPositon] = useState({ left: "50%", top: "50%" });
+    const [cursorShow, setCursorShow] = useState(false);
+    const cursorRef = useRef<HTMLDivElement>(null);
+
+    const mouseMove = (e: MouseEvent) => {
+        setCursorShow(true);
+        setPositon({ top: `${e.clientY}px`, left: `${e.clientX}px` });
+    };
+
+    const mouseLeave = () => {
+        setCursorShow(false);
+        setPositon({ left: "50%", top: "50%" });
+    };
+
+    useEffect(() => {
+        heroRef.current?.addEventListener("mousemove", mouseMove);
+        heroRef.current?.addEventListener("mouseleave", mouseLeave);
+
+        return () => {
+            heroRef.current?.removeEventListener("mousemove", mouseMove);
+            heroRef.current?.removeEventListener("mouseleave", mouseLeave);
+        };
+    }, []);
+
+    return (
+        <div
+            ref={cursorRef}
+            className={`absolute -translate-x-1/2 -translate-y-[40%] w-32 flex justify-center items-center flex-col text-center gap-4 font-bold text-sm cursor-none transition-opacity duration-300 uppercase ${
+                cursorShow ? "" : "transition-all"
+            }`}
+            style={{
+                top: positon.top,
+                left: positon.left,
+            }}
+            onClick={onClick}
+        >
+            <div className="w-28 aspect-square bg-light rounded-full flex justify-center items-center relative group overflow-hidden">
+                <span className="w-10 absolute group-hover:top-[0%] group-hover:opacity-0 transition-all duration-500 delay-75 top-1/2 -translate-y-1/2 ">
+                    {text}
+                </span>
+                <span className="w-10 absolute group-hover:top-1/2 group-hover:opacity-100 opacity-0 transition-all duration-500 delay-75 top-[70%] -translate-y-1/2">
+                    {text}
+                </span>
+            </div>
+            <div>{subtitle}</div>
+        </div>
+    );
+};
+
+export default HeroCursor;
